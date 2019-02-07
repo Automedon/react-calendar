62c437014a4e9b56f7ab9e80749feb474b6d8aa4
Valefar Frontend Developer <inzhener.intellect@gmail.com>
2019-02-07 20:58:27 +0700

    Add files via upload

---
+++ precache-manifest.cab06adc47c40c35bf4ce0a29ee3016a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "88ae6c84259b85ec310d",
+    "url": "/static/css/main.94a5fca9.chunk.css"
+  },
+  {
+    "revision": "88ae6c84259b85ec310d",
+    "url": "/static/js/main.88ae6c84.chunk.js"
+  },
+  {
+    "revision": "c8cc7a89b8863d67f161",
+    "url": "/static/js/1.c8cc7a89.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a3cfea7f8d2588348ffb42707049dfd9",
+    "url": "/index.html"
+  }
+];
